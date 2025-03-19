16a0a50ded8aeb222ad99f9b034a82a6940c0724
ZbigniewCement <177277@stud.prz.edu.pl>
2025-03-19 15:49:36 +0000

    2_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,20 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Random;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        Random random = new Random();
+        int suma = 0;
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+        // Generowanie 5 losowych liczb i ich suma
+        for (int i = 0; i < 5; i++) {
+            int liczba = random.nextInt(101); // Generowanie liczby z zakresu 0 - 100
+            suma += liczba;
+            System.out.println("Wylosowana liczba " + (i + 1) + ": " + liczba);
+        }
+
+        // Wypisanie sumy
+        System.out.println("Suma wylosowanych liczb: " + suma);
+    }
 }
+
+kalkulator który sprawdzi czy trójkąt jest prostokątny i obliczy jego 
